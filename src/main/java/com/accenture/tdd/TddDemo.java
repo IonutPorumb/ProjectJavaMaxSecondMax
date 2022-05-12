3fd21a1dbd619e0d5fe9bcc925f3802158335035
ionut.c.porumb <ionut_porumb@yahoo.com>
2022-05-12 08:51:45 +0300

    TddDemo class was modified to pass the test.

--- src/main/java/com/accenture/tdd/TddDemo.java
+++ src/main/java/com/accenture/tdd/TddDemo.java
@@ -6,12 +6,17 @@ public class TddDemo {
     public MaxValues max(List<Integer> list) {
         if (list != null && list.size() > 1) {
             int max = 0;
+            int secondMax = 0;
             for (int item : list) {
-                if (item > max) {
+                if (item > secondMax && item > max) {
+                    secondMax = max;
                     max = item;
                 }
+                if (item > secondMax && item < max) {
+                    secondMax = item;
+                }
             }
-            return new MaxValues(max, list.get(0));
+            return new MaxValues(max, secondMax);
         }
         return null;
     }
